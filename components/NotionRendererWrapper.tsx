0d590302f57b30caa5d1822c2e52f410d9617bc0
Kahhow Lee <kl1669@nyu.edu>
2025-04-26 22:46:21 +0800

    feat: upgrade notionrenderer for styling consistency

--- components/NotionRendererWrapper.tsx
+++ components/NotionRendererWrapper.tsx
@@ -1,5 +1,4 @@
 "use client";
-
 import dynamic from "next/dynamic";
 import { ExtendedRecordMap } from "notion-types";
 import "react-notion-x/src/styles.css";
@@ -16,11 +15,30 @@ export default function NotionRendererWrapper({
   recordMap: ExtendedRecordMap;
 }) {
   return (
-    <div className="notion-page prose prose-lg max-w-none">
+    <div>
       <NotionRenderer
         recordMap={recordMap}
         fullPage={false}
         darkMode={false}
+        components={{
+          nextImage: null,
+          image: ({ block }) => {
+            const source = block?.format?.display_source;
+            const alt = block?.properties?.caption?.[0]?.[0] ?? "";
+
+            if (!source) return null;
+
+            return (
+              <div className="my-6 flex justify-center">
+                <img
+                  src={source}
+                  alt={alt}
+                  className="rounded-xl shadow-md max-w-full h-auto"
+                />
+              </div>
+            );
+          },
+        }}
       />
     </div>
   );
